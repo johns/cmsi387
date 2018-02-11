0f6db0afb013a5b060e1692e7b519ec2ccaffe58
JohnS <johnrscott4@gmail.com>
2018-02-10 21:49:05 -0800

    Initial commit of Problem 4

---
+++ classwork2/l2d.c
@@ -0,0 +1,11 @@
+//Classwork 2: C-Lions Unite
+//Sam Chami, Sam Savini, and John Scott
+
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
+
+int main (int argc, char** argv){
+  argv[0] = "newlink";
+  FILE *fopen(argv, const char *mode);
+}
