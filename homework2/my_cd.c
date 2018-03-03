a20b792497ba19b6def988be1dd4aad4e004298f
johns <johnrscott4@gmail.com>
2018-03-02 18:00:03 -0800

    Got cd syscall to compile

--- homework2/my_cd.c
+++ homework2/my_cd.c
@@ -1,14 +1,14 @@
-#include <unistd.h>
-#include <string.h>
-#include <limits.h>
+#include <linux/limits.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main (int argc, char** argv) {
   char buffer[1024];
   char *path = realpath(argv[1], buffer);
 
   chdir(path);
-  getwd(buffer);
+  getenv(buffer);
   int bufferLength = strlen(buffer);
   buffer[bufferLength] = '\n';
   buffer[bufferLength+1] = '\0';
