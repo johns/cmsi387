e5ed18160d7115d73267c075abfd866d78282be8
johns <johnrscott4@gmail.com>
2018-03-02 23:38:59 -0800

    Reverted changes from recent commit

--- homework2/my_cat.c
+++ homework2/my_cat.c
@@ -5,14 +5,13 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-  const int BUFFER_MAX = 1024;
   for (int i = 1; i < argc; i++) {
     char abspath[PATH_MAX];
     realpath(argv[i], abspath);
     int fd = open(abspath, O_RDONLY);
-    char buffer[BUFFER_MAX];
-    while(read(fd, buffer, BUFFER_MAX) > 0) {
-      write(1, buffer, BUFFER_MAX);
+    char buffer[1024];
+    while(read(fd, buffer, 1024) > 0) {
+      write(1, buffer, 1024);
     }
   }
   return 0;
