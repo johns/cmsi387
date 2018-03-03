9d25d82d32304e9af84974c3b0f6246a5e22b436
johns <johnrscott4@gmail.com>
2018-03-02 17:48:20 -0800

    Got cat syscall to work

---
+++ homework2/my_cat.c
@@ -0,0 +1,20 @@
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main (int argc, char** argv) {
+  for (int i = 1; i < argc; i++) {
+    char abspath[PATH_MAX];
+    realpath(argv[i], abspath);
+    int fd = open(abspath, O_RDONLY);
+    char buffer[1024];
+    while(read(fd, buffer, 1024) > 0) {
+      write(1, buffer, 1024);
+    }
+  }
+}
