e89a65829e383c0f073c5c4be1fd90a1968dc84c
johns <johnrscott4@gmail.com>
2018-04-02 21:05:19 -0700

    Simplified syscall test

--- homework3/syscall_test.c
+++ homework3/syscall_test.c
@@ -1,21 +1,7 @@
-#include <stdlib.h>
-
-int supworld() {
-        int ret;
-        __asm__("movl $350,%eax");
-        __asm__("int $0x80");
-        __asm__("movl %eax, -4(%ebp)");
-        return ret;
-}
+#include <stddef.h>
+#include <unistd.h>
 
 int main()  {
-        int ret;
-        printf("invoking system call supworld...\n");
-        ret = supworld();
-        if(ret<0) {
-                printf("not working");
-                exit(1);
-        }
-        printf("system call executed.\n");
+        syscall(333, NULL);   // 333 is the supworld syscall index
         return 0;
 }
